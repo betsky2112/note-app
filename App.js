066c0d3c7dedb92ff4b6727926ccc52cf381cfe9
betsky2112 <roberthussebastians@gmail.com>
2024-05-22 22:29:34 +0700

    Develop button and textinput

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+import CustomButton from './src/components/customButton'
+import CustomTextInput from './src/components/customTextInput'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+	return (
+		<View>
+			<CustomButton
+				backgroundColor="#dddddd"
+				color="#3949f"
+				text="Custom Button"
+				width="100%"
+				onPress={() => {}}
+			/>
+			<CustomTextInput
+				label="Custom Text"
+				multiline
+				numberOfLines={2}
+				onChange={() => {}}
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		display: 'flex',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		padding: 40,
+	},
+})
+
+export default App
